86a64676bd88b1886196c303ad0154573cbf713c
T.J <thomas.turnage@noaa.gov>
2019-06-18 08:36:39 -0400

    Add files via upload

--- sat-the-final-chapter.py
+++ sat-the-final-chapter.py
@@ -117,7 +117,8 @@ def figure_timestamp(dt):
 
 def ltg_plot(highlow,ltg):
     """
-    Plots lightning
+    Plots lightning and assigns +/ based on polarity and color codes based
+    on ground stikes versus height of intercloud flash
     
     Parameters
     ----------
@@ -126,7 +127,7 @@ def ltg_plot(highlow,ltg):
         
     Returns
     -------
-    Nothing, just makes a scatterplot
+    Nothing, just makes a scatterplot then exits
                     
     """    
     for st in range(0,len(ltg)):
@@ -159,8 +160,10 @@ def ltg_plot(highlow,ltg):
         size = 10 + size_add
         if highlow == 'low' and hgt == 0:    
             a.scatter(lon,lat,s=size,marker=symb,c=col,zorder=zord)
+            a.set_title('EN Cloud to Ground')
         elif highlow == 'high' and hgt > 0:
             a.scatter(lon,lat,s=size,marker=symb,c=col,zorder=zord)
+            a.set_title('EN Intracloud')
     return
 
 import numpy as np
@@ -181,37 +184,49 @@ import pandas as pd
 from datetime import datetime
 #import matplotlib.pyplot as plt
 
-ltg_dir = 'C:/data/20190601/lightning'
-radar_stage_dir = 'C:/data/20190601/KGRR/stage'
-sat_source_dir = 'C:/data/20190601/satellite/raw'
-sat_stage_dir = 'C:/data/20190601/satellite/stage'
+base_dir = 'C:/data'
+event_date = '20190601'
+rda = 'KGRR'
+
+case_dir = os.path.join(base_dir,event_date)
+radar_dir = os.path.join(case_dir,rda,'netcdf/ReflectivityQC/00.50')
+sat_dir = os.path.join(case_dir,'satellite/raw')
+ltg_dir = os.path.join(case_dir,'lightning')
+image_dir = os.path.join(case_dir,'images')
+
 
 ltg_D = []
-radsat_D = []
-
-
 # lightning files obtained from EarthNetworks
 ltg_files = os.listdir(ltg_dir)
 ltg_csv = os.path.join(ltg_dir,ltg_files[0])
 ltg_D = pd.read_csv(ltg_csv, index_col=['time'])
 ltg_D.index = [datetime.strptime(x[:-2], '%Y-%m-%dT%H:%M:%S.%f') for x in ltg_D.index]
 
-# Here is a step where we decide to bin plots by time
-idx = pd.date_range('2019-06-01 22:15', periods=45, freq='1Min')
+# Here is a step where we define to bin plots by time
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+idx = pd.date_range('2019-06-01 22:12', periods=45, freq='1Min')
 dt = idx[1] - idx[0]
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
 
+# building a met_info list containing filepaths for all
+# radar and satellite (including GLM) products and their associated datetime
+# derived from filename convention
 met_info = []
 
-radar_files = os.listdir(radar_stage_dir)
+radar_files = os.listdir(radar_dir)
+#radar_files = os.listdir(radar_stage_dir)
 for r in (radar_files):
-    rad_info = str.split(r,'_')
+    rad_info = str.split(r,'.')
     rad_time_str = rad_info[0]
     rad_datetime = datetime.strptime(rad_time_str,"%Y%m%d-%H%M%S")
-    info = [rad_datetime,'r',os.path.join(radar_stage_dir,r)]
+    #info = [rad_datetime,'r',os.path.join(radar_stage_dir,r)]
+    info = [rad_datetime,'r',os.path.join(radar_dir,r)]
     met_info.append(info)
 
 
-satellite_files = os.listdir(sat_source_dir)
+satellite_files = os.listdir(sat_dir)
 for s in (satellite_files):
     sat_split = str.split(s,'_')
     dtype = sat_split[1][:3]
@@ -222,7 +237,7 @@ for s in (satellite_files):
     sat_time_s = sat_split[3]
     sat_time = sat_time_s[1:-1]
     sat_datetime = datetime.strptime(sat_time, "%Y%j%H%M%S")
-    info = [sat_datetime,g16_dtype,os.path.join(sat_source_dir,s)]
+    info = [sat_datetime,g16_dtype,os.path.join(sat_dir,s)]
     met_info.append(info)
 
 
@@ -231,24 +246,31 @@ np_met_info = np.array(met_info)
 metdat_D = pd.DataFrame(data=np_met_info[1:,1:],index=np_met_info[1:,0])  # 1st row as the column names
 metdat_D.columns = ['data_type', 'file_path']
 
-raddat = metdat_D[metdat_D.data_type == 'r']
-satdat = metdat_D[metdat_D.data_type == 's']
-glmdat = metdat_D[metdat_D.data_type == 'g']
-
+just_rad = (metdat_D.data_type == 'r')
+just_sat = (metdat_D.data_type == 's')
+just_glm = (metdat_D.data_type == 'g')
+ 
 file_sequence = []
 for i in range(0,len(idx)):
     new_datetime = idx[i]
+    #making progressively larger time slices
+    time_slice = (metdat_D.index < new_datetime)
     fig_title,fig_fname_tstr = figure_timestamp(new_datetime)
-    new_sat = satdat[satdat.index < new_datetime][-1:]
+
+    # the [-1:] slice grabs the latest (last) file to use for plotting
+    new_sat = metdat_D[time_slice & just_sat][-1:]
+    # file_path.max() is the only way I knew how to make xxx_path a string
     sat_path = new_sat.file_path.max()
 
-    new_rad = raddat[raddat.index < new_datetime][-1:]
+    new_rad = metdat_D[time_slice & just_rad][-1:]
     rad_path = new_rad.file_path.max()
-
-    new_glm = glmdat[glmdat.index < new_datetime][-1:]
+    new_glm = metdat_D[time_slice & just_glm][-1:]
     glm_path = new_glm.file_path.max()
 
-    new_seq = [sat_path,rad_path,fig_title,fig_fname_tstr,new_datetime,glm_path]
+    # building a list to define which filepaths to use for each timestep
+    # filepaths associated with longer time interval products
+    # can show up multiple times in successive timesteps
+    new_seq = [new_datetime,sat_path,rad_path,glm_path,fig_title,fig_fname_tstr]
     file_sequence.append(new_seq)
 
 base_gis_dir = 'C:/data/GIS'
@@ -272,19 +294,24 @@ plt.register_cmap(cmap=ir4_cmap)
 
 
 for fn in range(0,len(file_sequence)):
-    FILE = file_sequence[fn][0]
-    radar_file = file_sequence[fn][1]
-    fig_tstr = file_sequence[fn][2]
-    fig_fname_tstr = file_sequence[fn][3]
-    new_datetime = file_sequence[fn][4]
-    GLM = file_sequence[fn][5]
+    new_datetime = file_sequence[fn][0]
+    sat_file = file_sequence[fn][1]
+    radar_file = file_sequence[fn][2]
+    glm_file = file_sequence[fn][3]
     try:
-        G = xr.open_dataset(GLM)
+        G = xr.open_dataset(glm_file)
     except:
         pass
-    condition = (ltg_D.index > (idx[fn] - dt)) & (ltg_D.index <= idx[fn])
-    ltg = ltg_D[condition]
-    C = xr.open_dataset(FILE)
+
+    fig_title_tstr = file_sequence[fn][4]
+    fig_fname_tstr = file_sequence[fn][5]
+
+    # obtain lightning slice with time range between current date_range time
+    # and the time one time interval ago
+    ltg_time_slice = (ltg_D.index > (idx[fn] - dt)) & (ltg_D.index <= idx[fn])
+    ltg = ltg_D[ltg_time_slice]
+
+    # process radar file
     data = xr.open_dataset(radar_file)
     degrees_tilt = data.Elevation
     dnew2 = data.sortby('Azimuth')
@@ -295,6 +322,8 @@ for fn in range(0,len(file_sequence)):
     ref_arr = da.to_masked_array(copy=True)
     ra_filled = ref_arr.filled()
 
+    # process satellite file
+    C = xr.open_dataset(sat_file)
     gamma = 2.0
     
     C02 = C['CMI_C02'].data
@@ -320,7 +349,11 @@ for fn in range(0,len(file_sequence)):
     test = ['C02','Ref','C13', 'GLM','ltg_low','ltg_high']
     #test = ['C08','C13']
 
-    # Satellite longitude
+
+    # Now that all data arrays are created, we will begin plotting.
+    #
+    # First, determine satellite projection
+
     sat_lon = C['goes_imager_projection'].longitude_of_projection_origin
     sat_sweep = C['goes_imager_projection'].sweep_angle_axis
     sat_h = C['goes_imager_projection'].perspective_point_height
@@ -332,16 +365,16 @@ for fn in range(0,len(file_sequence)):
     # See details here: https://proj4.org/operations/projections/geos.html?highlight=geostationary
     x = C['x'][:] * sat_h
     y = C['y'][:] * sat_h
-
     # Create a pyproj geostationary map object
     p = Proj(proj='geos', h=sat_h, lon_0=sat_lon, a=semi_maj, b=semi_min, sweep=sat_sweep)
-    pc = ccrs.PlateCarree()
-    # Perform cartographic transformation. That is, convert image projection coordinates (x and y)
-    # to latitude and longitude values.
-    fig, axes = plt.subplots(2,3,figsize=(11,7),subplot_kw={'projection': ccrs.PlateCarree()})
-    plt.suptitle(fig_tstr)
+    #pc = ccrs.PlateCarree()
+
+    # Now perform cartographic transformation for satellite data.
+    # That is, convert image projection coordinates (x and y) to longtitudes/latitudess.
+    fig, axes = plt.subplots(2,3,figsize=(10,7),subplot_kw={'projection': ccrs.PlateCarree()})
+    plt.suptitle(fig_title_tstr)
     font = {'weight' : 'normal', 'size'   : 12}
-    plt.titlesize : 20
+    plt.titlesize : 24
 
     XX, YY = np.meshgrid(x, y)
     lons, lats = p(XX, YY, inverse=True)
@@ -353,6 +386,7 @@ for fn in range(0,len(file_sequence)):
     #arDict['C09'] = {'ar': C09, 'lat':lats, 'lon':lons}
     #arDict['C10'] = {'ar': C10, 'lat':lats, 'lon':lons}
     arDict['C13'] = {'ar': C13, 'lat':lats, 'lon':lons}
+    # Don't need to reproject radar data - already got lon/lats from 'latlon_from_radar' function
     arDict['Ref'] = {'ar': ra_filled, 'lat':rlats, 'lon':rlons}
     
     extent = [-86.4,-84.3,41.7,43.2]
@@ -368,6 +402,7 @@ for fn in range(0,len(file_sequence)):
 
         if str(y) == 'Ref':
             cs = a.pcolormesh(lat,lon,arr,cmap=plts[y]['cmap'],vmin=plts[y]['vmn'], vmax=plts[y]['vmx'])
+            a.set_title('Radar')
 
         elif str(y) == 'ltg_low':
             if len(ltg) > 0:
@@ -382,12 +417,18 @@ for fn in range(0,len(file_sequence)):
         elif str(y) == 'GLM':
             try:
                 a.scatter(G['flash_lon'], G['flash_lat'], marker='_')
+                a.set_title('GLM')
             except:
                 pass
         else:
             a.pcolormesh(lon,lat,arr,cmap=plts[y]['cmap'],vmin=plts[y]['vmn'],vmax=plts[y]['vmx'])
-
-    image_dst_path = os.path.join(sat_stage_dir,fig_fname_tstr + '.png')
+            if str(y) == 'C02':
+                a.set_title('Visible Satellite')
+            elif str(y) == 'C13':
+                a.set_title('Infrared Satellite')
+            else:
+                pass
+    image_dst_path = os.path.join(image_dir,fig_fname_tstr + '.png')
     plt.savefig(image_dst_path,format='png')
 
     plt.show()
